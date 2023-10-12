61b23875c0835e613fd1e5a2d1a851e2cdebc688
tisanti147 <santid147@gmail.com>
2023-10-12 14:50:25 -0300

    tp angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RegisterComponent } from './register/register.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginFormComponent,
+    NavbarComponent,
+    RegisterComponent,
+    HomePageComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
